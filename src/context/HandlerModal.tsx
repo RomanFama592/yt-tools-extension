b082e64061b48560c4a26439ae96af227515b1f9
Famá Román <famaroman@gmail.com>
2023-12-17 13:56:39 -0300

    docs: add documentation in HandlerModal

--- src/context/HandlerModal.tsx
+++ src/context/HandlerModal.tsx
@@ -1,33 +1,41 @@
-import { ReactNode, SyntheticEvent, createContext, useState, ReactElement } from "react";
+import { ReactNode, SyntheticEvent, createContext, useState, ReactElement, Dispatch, SetStateAction } from "react";
 
+// Define the properties expected in the context
 interface HandlerModalContextProps {
     modalState: boolean;
     modalContent: ReactNode;
-    setModalState: React.Dispatch<React.SetStateAction<boolean>>;
-    setModalContent: React.Dispatch<React.SetStateAction<ReactElement>>;
+    setModalState: Dispatch<SetStateAction<boolean>>;
+    setModalContent: Dispatch<SetStateAction<ReactElement>>;
     createHandlerModal: (elementValid: string[]) => (event: SyntheticEvent) => void;
 }
 
-export const ModalContext = createContext<HandlerModalContextProps | undefined>(undefined)
+// Create the context with the specified properties or undefined
+export const ModalContext = createContext<HandlerModalContextProps | undefined>(undefined);
 
+// Provider component that will wrap its children and provide the context values
 export function HandlerModalProvider({ children }: { children: ReactNode }) {
 
-    const [modalState, setModalState] = useState(false)
-    const [modalContent, setModalContent] = useState(<></>)
+    // State for managing modal visibility and content
+    const [modalState, setModalState] = useState(false);
+    const [modalContent, setModalContent] = useState(<></>);
 
+    // Function to create a modal event handler based on element validation
     const createHandlerModal = (elementValid: string[]) => {
         const handlerModal = (event: SyntheticEvent) => {
-            console.log(String(event.target))
+            // Check if the target is not included in the valid elements
             if (!elementValid.includes(String(event.target))) {
-                setModalState(true)
+                // Show the modal
+                setModalState(true);
             } else {
+                // Stop event propagation for valid elements
                 event.stopPropagation();
             }
-        }
+        };
 
-        return handlerModal
-    }
+        return handlerModal;
+    };
 
+    // Context value containing modal state, content, setters, and the modal event handler creator
     const contextValue: HandlerModalContextProps = {
         modalState,
         modalContent,
@@ -36,9 +44,10 @@ export function HandlerModalProvider({ children }: { children: ReactNode }) {
         createHandlerModal,
     };
 
+    // Provide the context to its children
     return (
         <ModalContext.Provider value={contextValue}>
             {children}
         </ModalContext.Provider>
-    )
+    );
 }
