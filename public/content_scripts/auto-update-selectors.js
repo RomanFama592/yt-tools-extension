2261576c1887319f6686bba2a001ab34bf72eb80
Famá Román <famaroman@gmail.com>
2023-12-18 01:40:53 -0300

    fix: add clause in case it does not exist

--- public/content_scripts/auto-update-selectors.js
+++ public/content_scripts/auto-update-selectors.js
@@ -5,6 +5,11 @@ const idsSelectors = namesSelectors.map(selector => CONSTANTS.initPartIdForSelec
 
 async function autoUpdateSelectors() {
     const newSelectors = await browser.storage.sync.get(idsSelectors)
+
+    if(!newSelectors){
+        return;
+    }
+
     namesSelectors.forEach((nameProp, index) => {
         SELECTORS[nameProp] = newSelectors[idsSelectors[index]]
             ? newSelectors[idsSelectors[index]]
